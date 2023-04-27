f11b61052855d66ef747b58c317d837d5b34c85a
pouria1337 <pouriadana73@gmail.com>
2023-04-27 21:18:46 +0330

    Remove magic numbers from encrypt.h source code and replace with symbolic constants.

--- encrypt.h
+++ encrypt.h
@@ -2,17 +2,19 @@
 #define ENCRYPT_F
 #include <stdio.h>
 
+#define ALPHABET_LEN 26
+
 char *encrypt_text(const char *, char *, int);
 
 char *encrypt_text(const char *source, char *destination, int key)
 {
     char *ch = destination;
     while(*source != '\0'){
-        if(65 <= *source && *source <= 90){
-            *destination = (*source - 65 + key) % 26 + 65;
+        if('A' <= *source && *source <= 'Z'){
+            *destination = (*source - 'A' + key) % ALPHABET_LEN + 'A';
         }
-        else if(97 <= *source && *source <= 122){
-            *destination = (*source - 97 + key) % 26 + 97;
+        else if('a' <= *source && *source <= 'z'){
+            *destination = (*source - 'a' + key) % ALPHABET_LEN + 'a';
         }
         else
             *destination = *source;
